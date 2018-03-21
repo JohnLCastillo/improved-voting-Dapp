f54ff72d7803b9059606f8c53fd60cdb11559d7e
John Castillo <JohnluisCastillo@gmail.com>
2018-03-21 17:11:45 -0400

    running locally

--- truffle.js
+++ truffle.js
@@ -8,7 +8,7 @@ module.exports = {
       port: 8545,
       network_id: '*',
       // from: '0x099801bff6bd0e8a02ba366a9e4bd77be1a5104c',
-      gas: 16777216
+      gas: 1400000
       // gasPrice: 25000000000
     }
   }
