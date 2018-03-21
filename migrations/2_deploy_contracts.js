56209f900061b85faf4a819ca15bbb2e88d3d144
John Castillo <JohnluisCastillo@gmail.com>
2018-03-21 14:51:03 -0400

    better contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var MetaCoin = artifacts.require("./MetaCoin.sol");
 var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Voting, ['Vernon', 'John', 'Hello Kitty'], {gas: 290000});
+  deployer.deploy(Voting, 10000, web3.toWei('0.01', 'ether'), ['Vernon', 'John', 'HelloKitty']);
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
