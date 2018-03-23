7dce802cc20a2078c0fdeba0b9a6ceba35b84794
John Castillo <JohnluisCastillo@gmail.com>
2018-03-23 10:33:19 -0400

    set up for deploy

--- truffle.js
+++ truffle.js
@@ -8,8 +8,13 @@ module.exports = {
       port: 8545,
       network_id: '*',
       // from: '0x099801bff6bd0e8a02ba366a9e4bd77be1a5104c',
-      gas: 1400000
-      // gasPrice: 25000000000
+      gas: 1400000,
+      gasPrice: 25000000000
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3"
     }
   }
 }
